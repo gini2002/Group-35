2e58558d449fd28b52a5e240f29aa23dc82d83f5
Ya Ni Zhao <yani.zhao@mail.utoronto.ca>
2023-11-03 12:57:04 -0400

    add SearchByName presenter, viewmodel, and state

---
+++ src/usecase_adaptor/SearchByNameViewModel.java
@@ -0,0 +1,18 @@
+package usecase_adaptor;
+
+public class SearchByNameViewModel {
+    private SearchByNameState state = new SearchByNameState();
+
+    private String name = "SearchByName";
+
+    public SearchByNameViewModel() {
+    }
+
+    public void setState(SearchByNameState state) {
+        this.state = state;
+    }
+
+    public SearchByNameState getState() {
+        return state;
+    }
+}
