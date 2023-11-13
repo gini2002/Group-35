d985c8532b999a657a7db26e1e2a657408a65fec
Ya Ni Zhao <yani.zhao@mail.utoronto.ca>
2023-11-13 15:09:53 -0500

    feature: add new use case Share Watchlist with other user

--- src/use_case/ShareWatchlist/ShareWatchlistInputData.java
+++ src/use_case/ShareWatchlist/ShareWatchlistInputData.java
@@ -1,2 +1,19 @@
-package use_case.ShareWatchlist;public class ShareWatchlistInputData {
+package use_case.ShareWatchlist;
+
+public class ShareWatchlistInputData {
+    private final String userName1;
+    private final String userName2;
+
+    public ShareWatchlistInputData(String name1, String name2) {
+        this.userName1 = name1;
+        this.userName2 = name2;
+    }
+
+    public String getSenderName() {
+        return userName1;
+    }
+
+    public String getReceiverName() {
+        return userName2;
+    }
 }
