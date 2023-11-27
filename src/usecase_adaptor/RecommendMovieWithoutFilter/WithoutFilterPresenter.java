734d37263a2b1dec19f08791aad73d5fb786002f
이채진 <ichaejin@ichaejins-mbp.home>
2023-11-27 05:22:44 -0500

    without filter presented created

--- src/usecase_adaptor/RecommendMovieWithoutFilter/WithoutFilterPresenter.java
+++ src/usecase_adaptor/RecommendMovieWithoutFilter/WithoutFilterPresenter.java
@@ -1,4 +1,36 @@
-package use_case.RecommendMovieWithoutFilter;
+package usecase_adaptor.RecommendMovieWithoutFilter;
 
-public class WithoutFilterPresenter {
+import use_case.RecommendMovieWithoutFilter.WithoutFilterOutputBoundary;
+import use_case.RecommendMovieWithoutFilter.WithoutFilterOutputData;
+import usecase_adaptor.ViewManagerModel;
+
+public class WithoutFilterPresenter implements WithoutFilterOutputBoundary {
+    private final WithoutFilterViewModel withoutFilterViewModel;
+
+    private final WithoutFilterResultViewModel withoutFilterResultViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public WithoutFilterPresenter(ViewManagerModel viewManagerModel, WithoutFilterViewModel withoutFilterViewModel, WithoutFilterResultViewModel withoutFilterResultViewModel) {
+        this.withoutFilterViewModel = withoutFilterViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.withoutFilterResultViewModel = withoutFilterResultViewModel;
+    }
+
+    @Override
+    public void WithoutFilterSuccessView(WithoutFilterOutputData outputData) {
+        withoutFilterViewModel.setWithoutFilterMovies(outputData.getWithoutFilterMovies());
+        withoutFilterViewModel.firePropertyChanged();
+        withoutFilterResultViewModel.setWithoutFilterMovies(outputData.getWithoutFilterMovies());
+        withoutFilterResultViewModel.firePropertyChanged();
+        this.viewManagerModel.setActiveView(withoutFilterViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void WithoutFilterFailView(String error) {
+        withoutFilterViewModel.setError(error);
+        withoutFilterViewModel.firePropertyChanged();
+
+    }
+
 }
