136a267750111ddbf0a91009c8d98a100abc1455
Ya Ni Zhao <yani.zhao@mail.utoronto.ca>
2023-11-27 18:25:14 -0500

    modified Login and Signup use case to adapt to our application.

--- src/usecase_adaptor/login/LoginPresenter.java
+++ src/usecase_adaptor/login/LoginPresenter.java
@@ -1,5 +1,7 @@
 package usecase_adaptor.login;
 
+import usecase_adaptor.MainMenu.MainMenuState;
+import usecase_adaptor.MainMenu.MainMenuViewModel;
 import usecase_adaptor.ViewManagerModel;
 import usecase_adaptor.logged_in.LoggedInState;
 import usecase_adaptor.logged_in.LoggedInViewModel;
@@ -8,28 +10,27 @@ import use_case.login.LoginOutputData;
 
 public class LoginPresenter implements LoginOutputBoundary {
 
+    private final MainMenuViewModel mainMenuViewModel;
     private final LoginViewModel loginViewModel;
-    private final LoggedInViewModel loggedInViewModel;
     private ViewManagerModel viewManagerModel;
 
     public LoginPresenter(ViewManagerModel viewManagerModel,
-                          LoggedInViewModel loggedInViewModel,
-                          LoginViewModel loginViewModel) {
+                          LoginViewModel loginViewModel,
+                          MainMenuViewModel mainMenuViewModel) {
         this.viewManagerModel = viewManagerModel;
-        this.loggedInViewModel = loggedInViewModel;
         this.loginViewModel = loginViewModel;
+        this.mainMenuViewModel = mainMenuViewModel;
     }
 
     @Override
     public void prepareSuccessView(LoginOutputData response) {
         // On success, switch to the logged in view.
 
-        LoggedInState loggedInState = loggedInViewModel.getState();
-        loggedInState.setUsername(response.getUsername());
-        this.loggedInViewModel.setState(loggedInState);
-        this.loggedInViewModel.firePropertyChanged();
+        MainMenuState mainMenuState = mainMenuViewModel.getState();
+        mainMenuState.setUserName(response.getUsername());
+        this.mainMenuViewModel.setState(mainMenuState);
 
-        this.viewManagerModel.setActiveView(loggedInViewModel.getViewName());
+        this.viewManagerModel.setActiveView(mainMenuViewModel.getViewName());
         this.viewManagerModel.firePropertyChanged();
     }
 
--- src/usecase_adaptor/signup/SignupViewModel.java
+++ src/usecase_adaptor/signup/SignupViewModel.java
@@ -1,20 +1,18 @@
 package usecase_adaptor.signup;
 
-import interface_adapter.ViewModel;
+import usecase_adaptor.ViewModel;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class SignupViewModel extends ViewModel {
 
-    public static final String CLEAR_BUTTON_LABEL = "Clear";
     public static final String TITLE_LABEL = "Sign Up View";
     public static final String USERNAME_LABEL = "Choose username";
     public static final String PASSWORD_LABEL = "Choose password";
     public static final String REPEAT_PASSWORD_LABEL = "Enter password again";
 
     public static final String SIGNUP_BUTTON_LABEL = "Sign up";
-    public static final String CANCEL_BUTTON_LABEL = "Cancel";
 
     private SignupState state = new SignupState();
 
