0ff52ec77c52b7c712ba70d1489a624f720c4538
이채진 <ichaejin@ichaejins-mbp.home>
2023-11-27 05:19:16 -0500

    without filter input data created

---
+++ src/use_case/RecommendMovieWithoutFilter/WithoutFilterInputData.java
@@ -0,0 +1,19 @@
+package use_case.RecommendMovieWithoutFilter;
+
+
+public class WithoutFilterInputData {
+    private final String watchlistId; // Identifier for the user's watchlist, if used separately
+
+    // Additional fields can be added as needed, for example, user preferences
+
+    // Constructor
+    public WithoutFilterInputData(String watchlistId) {
+        this.watchlistId = watchlistId;
+    }
+
+    // Getters for the fields
+
+    public String getWatchlistId() {
+        return watchlistId;
+    }
+}
