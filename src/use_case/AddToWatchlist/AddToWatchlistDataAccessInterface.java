b1bee384d327e8b0685c7488699cba32b91228b5
Ya Ni Zhao <yani.zhao@mail.utoronto.ca>
2023-11-30 10:45:54 -0500

    add java doc for use cases and entity.

--- src/use_case/AddToWatchlist/AddToWatchlistDataAccessInterface.java
+++ src/use_case/AddToWatchlist/AddToWatchlistDataAccessInterface.java
@@ -4,7 +4,18 @@ import entity.User;
 import entity.Movie;
 
 public interface AddToWatchlistDataAccessInterface {
+
+    /**
+     * save movie in user with useName's watchlist.
+     * @param userName of user who want to add movie.
+     * @param movie that is being added.
+     */
     void saveMovie(String userName, Movie movie);
 
+    /**
+     *
+     * @param userName of user who want to add movie.
+     * @return the user obeject correspond to user name.
+     */
     User getUser(String userName);
 }
--- src/use_case/AddToWatchlist/AddToWatchlistOutputBoundary.java
+++ src/use_case/AddToWatchlist/AddToWatchlistOutputBoundary.java
@@ -1,7 +1,16 @@
 package use_case.AddToWatchlist;
 
 public interface AddToWatchlistOutputBoundary {
+
+    /**
+     *
+     * @param error string of error message when add failed.
+     */
     void PrepareFailView(String error);
 
+    /**
+     *
+     * @param outputData that involve the movie being added.
+     */
     void PrepareSuccessView(AddToWatchlistOutputData outputData);
 }
