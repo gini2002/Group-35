c188aa566d19db1b37759b62cd3754134d0eaffa
이채진 <ichaejin@ichaejins-mbp.home>
2023-12-02 13:45:58 -0500

    modified to follow clean architecture rule

--- src/use_case/DeleteWatchlist/DeleteWatchlistDataAccessInterface.java
+++ src/use_case/DeleteWatchlist/DeleteWatchlistDataAccessInterface.java
@@ -4,9 +4,13 @@ import data_access.WatchlistDAO;
 import entity.Movie;
 import entity.User;
 
+import java.util.List;
+
 public interface DeleteWatchlistDataAccessInterface {
 
-    WatchlistDAO getWatchlistDAO();
+    public boolean removeMovieFromWatchlist(String username, int movieId);
+
+    public List<Integer> getWatchlistMoviesID(String name);
 //    Movie getMovie(Movie movie);
 
 }
