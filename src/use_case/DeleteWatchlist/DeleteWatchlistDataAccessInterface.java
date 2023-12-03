49bcb1910c68de86e73b8c36dfeb538f4428b250
이채진 <ichaejin@ichaejins-mbp.home>
2023-12-03 11:59:00 -0500

    get path added

--- src/use_case/DeleteWatchlist/DeleteWatchlistDataAccessInterface.java
+++ src/use_case/DeleteWatchlist/DeleteWatchlistDataAccessInterface.java
@@ -8,12 +8,14 @@ import java.util.List;
 
 public interface DeleteWatchlistDataAccessInterface {
 
-    public void removeMovieFromWatchlist(String username, int movieId);
+//    public void removeMovieFromWatchlist(String username, int movieId);
 
     //    public List<Integer> getWatchlistMoviesID(String name);
 //    Movie getMovie(Movie movie);
     User getUser(String userName);
 
     void saveMovie(String userName, Movie movie);
+
+    String getPath();
 }
 
