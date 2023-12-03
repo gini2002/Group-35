7cb353df9feae28cfd947cb038f132e032b4eb07
이채진 <ichaejin@ichaejins-mbp.home>
2023-12-03 15:17:59 -0500

    javadoc added

--- src/app/WithoutFilterResultUseCaseFactory.java
+++ src/app/WithoutFilterResultUseCaseFactory.java
@@ -16,9 +16,27 @@ import view.WithoutFilterResultView;
 
 import java.io.IOException;
 
+/**
+ * Factory class for creating instances related to the 'Without Filter Result' use case.
+ * This class serves as a central point for constructing the necessary components
+ * for the use case, ensuring that dependencies are correctly instantiated and injected.
+ */
+
 public class WithoutFilterResultUseCaseFactory {
 
     private WithoutFilterResultUseCaseFactory() {}
+
+    /**
+     * Creates a WithoutFilterResultView object with the necessary dependencies.
+     *
+     * @param viewManagerModel The view manager model for managing views.
+     * @param withoutFilterViewModel ViewModel for the without filter feature.
+     * @param withoutFilterResultViewModel ViewModel for the without filter result.
+     * @param getDetailMovieViewModel ViewModel for movie detail retrieval.
+     * @param detailMovieDataAccessInterface Data access interface for movie details.
+     * @return A new instance of WithoutFilterResultView.
+     * @throws Exception if there is an error during the creation process.
+     */
 
     public static WithoutFilterResultView create(
             ViewManagerModel viewManagerModel,
@@ -35,6 +53,19 @@ public class WithoutFilterResultUseCaseFactory {
             return null;
         }
     }
+
+    /**
+     * Private helper method to create a GetDetailMovieController.
+     * This method encapsulates the construction logic for the controller used in the movie detail retrieval feature.
+     *
+     * @param viewManagerModel The view manager model.
+     * @param withoutFilterViewModel ViewModel for without filter feature.
+     * @param withoutFilterResultViewModel ViewModel for the result of the without filter feature.
+     * @param getDetailMovieViewModel ViewModel for getting movie details.
+     * @param detailMovieDataAccessInterface Interface for movie detail data access.
+     * @return A new instance of GetDetailMovieController.
+     * @throws IOException if there is an IO error during the creation process.
+     */
 
     private static GetDetailMovieController createMovieResultUseCase(
             ViewManagerModel viewManagerModel,
