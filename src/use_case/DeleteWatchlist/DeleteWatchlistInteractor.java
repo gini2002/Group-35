c520e5110e88e345d773478cc04985486cce9ec2
이채진 <ichaejin@ichaejins-mbp.home>
2023-12-03 07:15:41 -0500

    csv reading and delete method changed

--- src/use_case/DeleteWatchlist/DeleteWatchlistInteractor.java
+++ src/use_case/DeleteWatchlist/DeleteWatchlistInteractor.java
@@ -95,35 +95,55 @@ package use_case.DeleteWatchlist;
 
 import data_access.WatchlistDAO;
 import entity.Movie;
+import entity.User;
 
 import java.util.List;
 
 public class DeleteWatchlistInteractor implements DeleteWatchlistInputBoundary {
     private final DeleteWatchlistOutputBoundary deleteWatchlistPresenter;
-    private final DeleteWatchlistDataAccessInterface deleteWatchListDAI;
+    private final DeleteWatchlistDataAccessInterface dataAccessInterface;
 
     public DeleteWatchlistInteractor(DeleteWatchlistOutputBoundary deleteWatchlistPresenter, DeleteWatchlistDataAccessInterface dataAccessInterface) {
         this.deleteWatchlistPresenter = deleteWatchlistPresenter;
-        this.deleteWatchListDAI = dataAccessInterface;
+        this.dataAccessInterface = dataAccessInterface;
     }
 
     @Override
     public void execute(DeleteWatchlistInputData inputData) {
+        User user = dataAccessInterface.getUser(inputData.getUserName());
         String username = inputData.getUserName();
         Movie movie = inputData.getMovie();
-        List<Integer> all_movie = deleteWatchListDAI.getWatchlistMoviesID(username);
+        List<Movie> watchList = user.getWatchlist();
+        boolean movieFound = false;
 
-        if (all_movie.contains(movie.getID())) {
-            deleteWatchListDAI.removeMovieFromWatchlist(username, movie.getID());
-            DeleteWatchlistOutputData outputData = new DeleteWatchlistOutputData(movie);
-            deleteWatchlistPresenter.PrepareSuccessView(outputData);
-        } else {
-
-            deleteWatchlistPresenter.PrepareFailView(movie.getName() + " doesn't exists");
-
+        for (Movie movies : watchList) {
+            if (movies.getID() == movie.getID()) {
+                movieFound = true;
+                dataAccessInterface.saveMovie(inputData.getUserName(), movie);
+                DeleteWatchlistOutputData outputData = new DeleteWatchlistOutputData(movie);
+                deleteWatchlistPresenter.PrepareSuccessView(outputData);
+                break;  // This will exit the loop once the matching movie is found
+            }
         }
+        if (!movieFound) {
+            deleteWatchlistPresenter.PrepareFailView(movie.getName() + " doesn't exist");
+        }
+//        String username = inputData.getUserName();
+//        Movie movie = inputData.getMovie();
+//        List<Integer> all_movie = deleteWatchListDAI.getWatchlistMoviesID(username);
+//
+//        if (all_movie.contains(movie.getID())) {
+//            deleteWatchListDAI.removeMovieFromWatchlist(username, movie.getID());
+//            DeleteWatchlistOutputData outputData = new DeleteWatchlistOutputData(movie);
+//            deleteWatchlistPresenter.PrepareSuccessView(outputData);
+//        } else {
+//
+//            deleteWatchlistPresenter.PrepareFailView(movie.getName() + " doesn't exists");
+
+
     }
 }
+
 
 
 
