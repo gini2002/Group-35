14795f8ba5fab6b4f1788401f17e8258b84466da
이채진 <ichaejin@ichaejins-mbp.home>
2023-12-03 16:42:34 -0500

    javadoc added

--- src/use_case/DeleteWatchlist/DeleteWatchlistInputData.java
+++ src/use_case/DeleteWatchlist/DeleteWatchlistInputData.java
@@ -2,9 +2,22 @@ package use_case.DeleteWatchlist;
 
 import entity.Movie;
 
+/**
+ * Represents the input data for the delete watchlist use case.
+ * This class encapsulates the data required to delete a movie from a user's watchlist,
+ * including the specific movie and the username of the user.
+ */
+
 public class DeleteWatchlistInputData {
     private final Movie movie;
     private final String username;
+
+    /**
+     * Constructs a new DeleteWatchlistInputData object.
+     *
+     * @param movie The Movie object to be deleted from the watchlist.
+     * @param username The username of the user whose watchlist will be modified.
+     */
 
     public DeleteWatchlistInputData(Movie movie, String username) {
 
@@ -12,9 +25,21 @@ public class DeleteWatchlistInputData {
         this.username = username;
     }
 
+    /**
+     * Gets the Movie object.
+     *
+     * @return The Movie object to be deleted from the watchlist.
+     */
+
     public Movie getMovie() {
         return movie;
     }
+
+    /**
+     * Gets the username of the user.
+     *
+     * @return The username of the user whose watchlist is being modified.
+     */
 
     public String getUserName() {return username;}
 }
